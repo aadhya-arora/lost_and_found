731c0ee3f8a5ef6db2232105e8a94e6a0f03db80
aadhya-arora <aadhya270805@gmail.com>
2025-07-09 20:55:03 +0530

    did some work on report page

--- lf/src/pages/report.tsx
+++ lf/src/pages/report.tsx
@@ -1,5 +1,7 @@
 import "../styling/report.css";
 import { FaSearch } from "react-icons/fa";
+import { HiOutlineExclamation } from "react-icons/hi";
+import { HiOutlineCheckCircle } from "react-icons/hi";
 
 const Report = () => {
   return (
@@ -13,8 +15,42 @@ const Report = () => {
           Post your lost or found item and connect with trusted finders
         </p>
         <div className="report-choose">
-          <button className="report-found">Found an item</button>
-          <button className="report-lost">Lost an item</button>
+          <button className="report-found">
+            <HiOutlineCheckCircle size={30} /> <br />
+            Found an item
+          </button>
+          <button className="report-lost">
+            <HiOutlineExclamation size={30} /> <br />
+            Lost an item
+          </button>
+        </div>
+      </div>
+      <div className="file-report">
+        <div className="progress-bar">
+          <div className="progress1"></div>
+          <div className="progress2"></div>
+          <div className="progress3"></div>
+        </div>
+        <div className="report-lost-item-section">
+          <h1 className="lost-report-heading">Report a Lost Item</h1>
+          <label htmlFor="item-name">Name</label>
+          <input
+            className="lost-item-name"
+            type="text"
+            id="item-name"
+            placeholder="What have you lost?"
+            required
+          />
+          <label htmlFor="item-category">Category</label>
+          <input
+            className="lost-category"
+            type="text"
+            id="item-category"
+            placeholder="Category..."
+            required
+          />
+          <label htmlFor="lost-description">Description</label>
+          <textarea className="lost-description"></textarea>
         </div>
       </div>
     </div>
