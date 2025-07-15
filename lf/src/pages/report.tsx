ba3ace886b341bd185d7f3d07f5a03fc277de35f
aadhya-arora <aadhya270805@gmail.com>
2025-07-15 19:22:24 +0530

    found-form

--- lf/src/pages/report.tsx
+++ lf/src/pages/report.tsx
@@ -32,6 +32,20 @@ interface FormDataType {
   email: string;
 }
 
+interface FoundDataType {
+  name: string;
+  color: string;
+  brand: string;
+  uniqueId: string;
+  dateFound: string;
+  image: File | null;
+  imagePreview: string | null;
+  location: string;
+  category: string;
+  phone: string;
+  email: string;
+}
+
 interface LocationMarkerProps {
   setFormData: React.Dispatch<React.SetStateAction<FormDataType>>;
   formData: FormDataType;
@@ -72,7 +86,21 @@ const Report: React.FC = () => {
     image: null,
     imagePreview: null,
     location: "",
-    category: "Bag (AI Suggested)",
+    category: "Bag",
+    phone: "",
+    email: "",
+  });
+
+  const [foundData, setFoundData] = useState<FoundDataType>({
+    name: "",
+    color: "",
+    brand: "",
+    uniqueId: "",
+    dateFound: "",
+    image: null,
+    imagePreview: null,
+    location: "",
+    category: "",
     phone: "",
     email: "",
   });
@@ -144,6 +172,220 @@ const Report: React.FC = () => {
           </button>
         </div>
       </div>
+      {selectedOption === "found" && (
+        <div className="found-report">
+          <div className="progress-bar">
+            <div
+              className="progress1"
+              style={{ backgroundColor: step >= 1 ? "#4f772d" : "#cbd5c0" }}
+            ></div>
+            <div
+              className="progress2"
+              style={{ backgroundColor: step >= 2 ? "#4f772d" : "#cbd5c0" }}
+            ></div>
+            <div
+              className="progress3"
+              style={{ backgroundColor: step >= 3 ? "#4f772d" : "#cbd5c0" }}
+            ></div>
+          </div>
+          <div className="report-found-item-section">
+            <h1 className="found-report-heading">Report a Found Item</h1>
+            <form className="found-form">
+              {step === 1 && (
+                <>
+                  <label htmlFor="name" className="label-headings">
+                    What do you find?
+                  </label>
+                  <input
+                    className="found-item-name"
+                    type="text"
+                    id="name"
+                    placeholder="Backpack"
+                    value={foundData.name}
+                    onChange={handleChange}
+                    required
+                  />
+                  <label htmlFor="color" className="label-headings">
+                    Color
+                  </label>
+                  <input
+                    className="found-item-name"
+                    type="text"
+                    id="color"
+                    placeholder="Black"
+                    value={foundData.color}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="brand" className="label-headings">
+                    Brand(optional)
+                  </label>
+                  <input
+                    className="found-item-name"
+                    type="text"
+                    id="brand"
+                    placeholder="Wildcraft"
+                    value={foundData.brand}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="uniqueId" className="label-headings">
+                    Unique Identification (optional)
+                  </label>
+                  <input
+                    className="found-item-name"
+                    type="text"
+                    id="uniqueId"
+                    placeholder="E.g., sticker inside pocket"
+                    value={foundData.uniqueId}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="dateFound" className="label-headings">
+                    Date Found
+                  </label>
+                  <input
+                    className="found-item-name"
+                    type="date"
+                    id="dateFound"
+                    value={foundData.dateFound}
+                    onChange={handleChange}
+                    required
+                  />
+                  <button
+                    type="button"
+                    className="submit-report-button"
+                    onClick={handleNext}
+                  >
+                    Next
+                  </button>
+                </>
+              )}
+              {step === 2 && (
+                <>
+                  <label htmlFor="image" className="label-headings">
+                    Upload Image
+                  </label>
+                  <div className="file-input-wrapper">
+                    <button type="button" className="custom-file-button">
+                      Browse
+                    </button>
+                    <input
+                      type="file"
+                      id="image"
+                      accept="image/*"
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+
+                  {foundData.imagePreview && (
+                    <div className="image-preview-wrapper">
+                      <img
+                        src={foundData.imagePreview}
+                        alt="Preview"
+                        className="image-preview"
+                      />
+                      <button
+                        type="button"
+                        className="remove-image-cross"
+                        onClick={handleRemoveImage}
+                      >
+                        {" "}
+                        ✖
+                      </button>
+                    </div>
+                  )}
+                  <label className="label-headings2">
+                    Drop a Pin on the Map
+                  </label>
+                  <MapContainer
+                    center={[30.516, 76.6597]}
+                    zoom={15}
+                    style={{
+                      height: "300px",
+                      width: "100%",
+                      marginBottom: "15px",
+                    }}
+                  >
+                    <TileLayer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <LocationMarker
+                      setFormData={setFormData}
+                      formData={formData}
+                    />
+                  </MapContainer>
+                  {foundData.location && (
+                    <p>
+                      <strong>Selected Location:</strong>
+                      {foundData.location}
+                    </p>
+                  )}
+                  <div style={{ display: "flex", gap: "10px" }}>
+                    <button
+                      type="button"
+                      className="submit-report-button"
+                      onClick={handleBack}
+                    >
+                      Back
+                    </button>
+                    <button
+                      type="button"
+                      className="submit-report-button"
+                      onClick={handleNext}
+                    >
+                      Next
+                    </button>
+                  </div>
+                </>
+              )}
+              {step === 3 && (
+                <>
+                  <h3>Suggested Category</h3>
+                  <p style={{ marginTop: "-5px" }}>
+                    <strong>Category:</strong> {formData.category}
+                  </p>
+                  <label htmlFor="phone" className="label-headings">
+                    Phone Number
+                  </label>
+                  <input
+                    className="lost-item-name"
+                    type="tel"
+                    id="phone"
+                    placeholder="Enter your phone number"
+                    value={foundData.phone}
+                    onChange={handleChange}
+                    required
+                  />
+                  <label htmlFor="email" className="label-headings">
+                    Email
+                  </label>
+                  <input
+                    className="lost-item-name"
+                    type="email"
+                    id="email"
+                    placeholder="Enter your email"
+                    value={foundData.email}
+                    onChange={handleChange}
+                    required
+                  />
+                  <div style={{ display: "flex", gap: "10px" }}>
+                    <button
+                      type="button"
+                      className="submit-report-button"
+                      onClick={handleBack}
+                    >
+                      Back
+                    </button>
+                    <button type="submit" className="submit-report-button">
+                      Report
+                    </button>
+                  </div>
+                </>
+              )}
+            </form>
+          </div>
+        </div>
+      )}
       {selectedOption === "lost" && (
         <div className="file-report">
           <div className="progress-bar">
@@ -167,13 +409,13 @@ const Report: React.FC = () => {
               {step === 1 && (
                 <>
                   <label htmlFor="name" className="label-headings">
-                    Name of Item
+                    What did you lose?
                   </label>
                   <input
                     className="lost-item-name"
                     type="text"
                     id="name"
-                    placeholder="E.g., Black Backpack"
+                    placeholder="Backpack"
                     value={formData.name}
                     onChange={handleChange}
                     required
@@ -251,13 +493,18 @@ const Report: React.FC = () => {
                   <label htmlFor="image" className="label-headings">
                     Upload Image
                   </label>
-                  <input
-                    type="file"
-                    id="image"
-                    accept="image/*"
-                    onChange={handleChange}
-                    required
-                  />
+                  <div className="file-input-wrapper">
+                    <button type="button" className="custom-file-button">
+                      Browse
+                    </button>
+                    <input
+                      type="file"
+                      id="image"
+                      accept="image/*"
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
 
                   {formData.imagePreview && (
                     <div className="image-preview-wrapper">
@@ -325,7 +572,7 @@ const Report: React.FC = () => {
 
               {step === 3 && (
                 <>
-                  <h3>AI-Suggested Category</h3>
+                  <h3>Category</h3>
                   <p>
                     <strong>Category:</strong> {formData.category}
                   </p>
@@ -365,7 +612,7 @@ const Report: React.FC = () => {
                       Back
                     </button>
                     <button type="submit" className="submit-report-button">
-                      Submit Report
+                      Report
                     </button>
                   </div>
                 </>
